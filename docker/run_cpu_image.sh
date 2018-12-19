9aadc89a9ad2f91e426826b1fa8a1ddfe7d5076a
Lionel <lionel.blonde@gmail.com>
2018-12-19 14:10:24 +0100

    Modify dockerfiles and docker runners

--- docker/run_cpu_image.sh
+++ docker/run_cpu_image.sh
@@ -1,3 +1,7 @@
+cd ..
+
 docker run -i -t --rm -v $(pwd)/MJKEY:/MJKEY \
-                      -v $(pwd)/DEMOS:/DEMOS \
-                      -v $(pwd)/data:/code/sam-tf/data docker-sam-tf-cpu:latest bash
+                      -v $(pwd)/DEMOS:/code/DEMOS \
+                      -v $(pwd)/data:/code/data \
+                      -v $(pwd)/imitation:/code/imitation \
+                      -v $(pwd)/launchers:/code/launchers docker-sam-tf-cpu:latest bash
